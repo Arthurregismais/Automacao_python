6de7c4f541defab0d597f8780f10b1e058c26c65
Arthur <arthur.regismais@hotmail.com>
2021-12-07 15:50:22 -0300

    Initial commit

---
+++ Automação.py
@@ -0,0 +1,32 @@
+import pyautogui
+import time
+
+if __name__ == '__main__':
+    pyautogui.alert('Por favor, não mexa no teclado ou no mouse durante a execução do código')
+    pyautogui.PAUSE = 0.5
+#Abrir o google drive no meu computador
+
+    pyautogui.press('winleft')
+    pyautogui.write('chrome')
+    pyautogui.press('enter')
+    time.sleep(1)
+    pyautogui.write('https://drive.google.com/drive/u/0/my-drive')
+    pyautogui.press('enter')
+
+#Entrar na àrea de trabalho
+    pyautogui.hotkey('winleft', 'd')
+    pyautogui.moveTo(180, 443)
+    pyautogui.mouseDown()
+    pyautogui.moveTo(757, 582)
+
+#Enquanto arrasto, mudo para o google drive
+    pyautogui.hotkey('alt', 'tab')
+
+#largando o arquivo no drive
+    pyautogui.mouseUp()
+
+#Esperar 5 segundos
+    time.sleep(5)
+    pyautogui.alert('O código acabou de rodar, pode usar seu computador novamente ')
+
+
